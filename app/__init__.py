4fc4f2b9852955a9d6b7c52b02a7f400617d4b7f
simonlombardi <s.lombardi@itecriocuarto.org.ar>
2023-09-26 17:22:04 -0300

    fix gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)   
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:DB2021itec@143.198.156.171/Lombardi2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://DB2021:DB2021itec@143.198.156.171/Lombardi2'
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI') 
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
 
